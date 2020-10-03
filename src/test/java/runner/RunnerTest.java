b0c8b6d417a7be3555bfd8a89796cf57164b9c56
Victoria441 <v.abudinenvelarde@gmail.com>
2020-10-02 22:45:32 -0400

    Login Page test

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -11,7 +11,7 @@ import org.testng.annotations.DataProvider;
                 "json:target/cucumber.json",
                 "rerun:target/re-run.txt"
         },
-        tags = "@adminPage"
+        tags = "@loginPage"
         ,dryRun = false
 )
 public class RunnerTest extends AbstractTestNGCucumberTests {
