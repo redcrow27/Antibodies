278a86d06a242b82b47aebe469b21672ece8f50f
srmazur <63271426+srmazur@users.noreply.github.com>
2020-10-03 12:17:00 -0400

    Pojo class was created With getters and setters

--- src/test/java/pojo/UserForm.java
+++ src/test/java/pojo/UserForm.java
@@ -1,4 +1,45 @@
 package pojo;
 
+import com.github.javafaker.Faker;
+
 public class UserForm {
+
+    private String id;
+    private String FirstName;
+    private String LastName;
+    Faker faker = new Faker();
+
+    public UserForm(String id, String firstName, String lastName) {
+        this.id = id;
+        this.FirstName = firstName;
+        this.LastName = lastName;
+    }
+
+    public UserForm() {
+
+    }
+
+    public String getId() {
+        return faker.idNumber().valid();
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return faker.name().firstName();
+    }
+
+    public void setFirstName(String firstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return faker.name().lastName();
+    }
+
+    public void setLastName(String lastName) {
+        this.LastName = lastName;
+    }
 }
