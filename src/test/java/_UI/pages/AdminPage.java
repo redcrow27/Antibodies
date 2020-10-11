50214f07e09d30c846460733e502423fdee13a02
srmazur <63271426+srmazur@users.noreply.github.com>
2020-10-11 15:07:40 -0400

    Web Elements was added to AdminPage page fuctory class

--- src/test/java/_UI/pages/AdminPage.java
+++ src/test/java/_UI/pages/AdminPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 
 import java.nio.file.WatchEvent;
+import java.util.List;
 
 /**
  * All WebElements, also known as page objects of Admin Page are stored in this class. Page Factory is used to initialize
@@ -17,13 +18,12 @@ import java.nio.file.WatchEvent;
 public class AdminPage {
 
 
-
-    public AdminPage(WebDriver driver){
+    public AdminPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
 
     /**
-     *  Example creating element
+     * Example creating element
      */
     @FindBy(id = "valueOfId")
     public WebElement sampleElement;
@@ -31,49 +31,67 @@ public class AdminPage {
     @FindBy(xpath = "//table[@class='table']/thead")
     public WebElement adminTable;
 
-   public String[] employeeData = {"ID", "First", "Last", "Role", "Department"};
+    public String[] employeeData = {"ID", "First", "Last", "Role", "Department"};
 
-   @FindBy(xpath = "//button[text()='Copy Token']")
+    @FindBy(xpath = "//button[text()='Copy Token']")
     public WebElement copyTokenBtn;
 
-   @FindBy(xpath = "//form/input[@id='inputArea1']")
+    @FindBy(xpath = "//form/input[@id='inputArea1']")
     public WebElement createRole;
 
-   @FindBy(xpath = "//*[contains(text(),'Add')]")
+    @FindBy(xpath = "//*[contains(text(),'Add')]")
     public WebElement addButton;
 
-   @FindBy(xpath = "//button[@class='btn btn-primary roleDelete']")
+    @FindBy(xpath = "//button[@class='btn btn-primary roleDelete']")
     public WebElement DeleteRole;
 
-   //Marina
-   @FindBy ( id="inputArea1")
+    //Marina
+    @FindBy(id = "inputArea1")
     public WebElement enterRole;
 
-   @FindBy(xpath = "//*[@id=\"root\"]/div[1]/div[2]/div/div[1]/form/button")
+    @FindBy(xpath = "//*[@id=\"root\"]/div[1]/div[2]/div/div[1]/form/button")
     public WebElement addButton1;
 
-   @FindBy(id="inputArea2")
-           public WebElement departmentAreaRow;
+    @FindBy(id = "inputArea2")
+    public WebElement departmentAreaRow;
 
-   @FindBy(xpath ="//*[@id=\"root\"]/div[1]/div[2]/div/div[2]/form/button")
+    @FindBy(xpath = "//*[@id=\"root\"]/div[1]/div[2]/div/div[2]/form/button")
     public WebElement addClickButton;
 
-   @FindBy(name="username")
-    public  WebElement username;
+    @FindBy(name = "username")
+    public WebElement username;
 
-   @FindBy(name="password")
+    @FindBy(name = "password")
     public WebElement password;
 
-   @FindBy(xpath ="//*[@id=\"root\"]/div/div/div/form/button")
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div/form/button")
     public WebElement signIn;
 
-   @FindBy(css = "div.RoleClass")
+    @FindBy(css = "div.RoleClass")
     public WebElement tableRow;
 
 
+    // **********************
 
+    @FindBy(xpath = "//div[@class='dep']//tr/td/span")
+    public List<WebElement> department_table_count;
+    @FindBy(xpath = "//button[@class='btn btn-primary roleDelete']")
+    public List<WebElement> role_delete;
 
+    @FindBy(xpath = "//div[1]/div[2]/div/div[1]//tr/td/span")
+    public List<WebElement> role_table_count;
+    @FindBy(xpath = "//div[@class='dep']//button[@class='btn btn-primary ml-2 roleDelete']")
+    public List<WebElement> department_delete;
 
+    @FindBy(xpath = "//*[@id='myModal']/div/div/h3")
+    public WebElement warning_popup;
+    @FindBy(xpath = "//*[@id='myModal']/div/div/h3")
+    public WebElement wrpop;
+
+    @FindBy(id = "submit-btn")
+    public WebElement warning_close;
+
+    //**********************************
 
 
 }
